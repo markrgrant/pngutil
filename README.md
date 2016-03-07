0c67768cffd2865581254700899a1f261d6548bc
Mark Grant <markrgrant@yahoo.com>
2016-03-07 05:54:39 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# README
+
+A simple PNG encoding/decoding library using attoparsec.
+
+## References
+
+http://www.libpng.org/pub/png/spec/iso/index-object.html
---
+++ test/TestPNG.hs
@@ -0,0 +1,20 @@
+import PNG
+import System.Exit (exitSuccess, exitFailure)
+
+
+tests :: [Bool]
+tests = []
+
+
+runTests tests = do
+    if (and tests)
+        then exitSuccess
+        else do
+            print tests
+            exitFailure
+
+
+main :: IO ()
+main = do
+    runTests tests
+    return ()
