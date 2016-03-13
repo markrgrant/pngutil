4c9f467c1f38720394b2e4ccd0d177636658c36b
Mark Grant <markrgrant@yahoo.com>
2016-03-12 22:30:51 -0800

    add crc calculation function

---
+++ CRC32.hs
@@ -0,0 +1,27 @@
+-- Table of CRCs of all 8-bit messages
+
+import qualified Data.Vector as V -- for O(1) lookup
+import Data.Bits
+import Data.Word
+
+
+-- a table containing all 8-bit messages in order to improve the
+-- performance of the calculation of a CRC. 
+crcTable :: V.Vector Word64
+crcTable = V.fromList $ map makeCRCEntry [0..255]
+    where makeCRCEntry n = foldl (\acc _ -> nextCRC acc) n [0..7]
+          nextCRC c
+              | c .&. 1 == 0 = c `shiftR` 1
+              | otherwise = 0xedb88320 `xor` (c `shiftR` 1)
+
+
+updateCRC :: V.Vector Word64 -> [Word8] -> Word64
+updateCRC table bytes = foldl func crc bytes
+    where crc = 0xffffffff -- initial crc is all 1's
+          func acc byte = (table V.! index) `xor` (acc `shiftR` 8)
+              where index = fromIntegral (acc `xor` fromIntegral byte) .&. 0xff :: Int
+
+
+-- Return the 1's complement of the computed crc
+crc :: V.Vector Word64 -> [Word8] -> Word64
+crc table bytes = updateCRC table bytes `xor` 0xffffffff
